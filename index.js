b1ba07c8ef84eec8a772883ee3873cf376849bc6
cloudedpanther <rickeykoh@ajou.ac.kr>
2021-02-25 14:36:33 +0900

    forgot to include

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const app = express();
 
-// respond with "hello world" when a GET request is made to the homepage
-app.get('/', function(req, res) {
-  res.send('hello world');
-});
+const PORT = 4000;
+
+function handleListening() {
+  console.log(`Listening on: http://localhost:${PORT}`)
+}
+
+app.listen(PORT, handleListening);
