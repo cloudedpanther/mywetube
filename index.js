46a598101ec9457d4171789a684ab784bc43c7a1
cloudedpanther <rickeykoh@ajou.ac.kr>
2021-02-25 14:35:17 +0900

    first server open

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-console.log("Hello, there.");
+const express = require('express');
+const app = express();
+
+// respond with "hello world" when a GET request is made to the homepage
+app.get('/', function(req, res) {
+  res.send('hello world');
+});
